04259409d53ea0baecad2283b3f3f54d2f7bf2be
ramon pereira do nascimento <88009771+ramon-n@users.noreply.github.com>
2023-05-08 17:44:00 -0300

    Add files via upload

---
+++ src/main/java/conversorfahrenheitcelsius/Conversor.java
@@ -0,0 +1,10 @@
+package conversorfahrenheitcelsius;
+
+public class Conversor {
+	public double fahrenheitToCelsius(double grausF) {
+		double grausC = 0.0;
+		return grausC = (grausF - 32) / 1.8;
+	}
+	
+
+}
